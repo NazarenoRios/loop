419ddbcbe4b34b7e75aabb4902d1d62f6de45faa
NazarenoRios <nazarenolrios@gmail.com>
2023-08-17 18:51:07 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [xd, setXd] = useState(2);
+
+  for (let i = 0; i < xd; i--) {
+    console.log("XD");
+  }
+
+  return <div className="App">Thanks for your ip 😊</div>;
 }
 
 export default App;
